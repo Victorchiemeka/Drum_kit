0dad77995f9754d81ec104847303a47dd4dbc2d3
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-12 18:46:26 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+
+// variable for the loop
 var x;
+
+// declaring a variable that selects the class drum
 var drums = document.querySelectorAll(".drum");
 
+//a loop that iterates through all the drum classes and using the length to get the numbers
 for (x = 0; x < drums.length; x++) {
+  
+  //selecting all the items in the drum class by putting in an array
+  //and adding an event listener on click to the drum class 
   drums[x].addEventListener("click", function() {
+
+    //declared a variable that stores the element between opening and closing tag
     var buttonInnerHTML = this.innerHTML;
 
+    // a conditional statement that check for each element clicked 
     switch (buttonInnerHTML) {
+      
+      // all this are statements in the condition that add sound to differnt drum clicked 
       case "w":
         var tom1 = new Audio('sounds/tom-1.mp3');
         tom1.play();
@@ -39,3 +52,4 @@ for (x = 0; x < drums.length; x++) {
     }
   });
 }
+
